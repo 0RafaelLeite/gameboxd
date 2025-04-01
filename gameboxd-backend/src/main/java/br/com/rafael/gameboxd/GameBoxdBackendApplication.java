69567ac4bf73215ff838cb57d594ac120273c7d6
Rafael Leite <rafael.leite@itlab.com.br>
2025-03-31 22:13:43 -0300

    initial commmit

---
+++ gameboxd-backend/src/main/java/br/com/rafael/gameboxd/GameBoxdBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.rafael.gameboxd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameBoxdBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameBoxdBackendApplication.class, args);
+	}
+
+}

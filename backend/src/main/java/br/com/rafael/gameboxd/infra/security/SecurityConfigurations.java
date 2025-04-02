ef0277c61972b6a76835fa25abe45313f31cdde9
Rafael Leite <rafaelrocha.git@outlook.com>
2025-04-02 00:22:01 -0300

    wip

--- backend/src/main/java/br/com/rafael/gameboxd/infra/security/SecurityConfigurations.java
+++ backend/src/main/java/br/com/rafael/gameboxd/infra/security/SecurityConfigurations.java
@@ -2,6 +2,7 @@ package br.com.rafael.gameboxd.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -15,7 +16,7 @@ public class SecurityConfigurations {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.POST).hasRole("ADMIN").anyRequest().authenticated())
                 .build();
     }
 }

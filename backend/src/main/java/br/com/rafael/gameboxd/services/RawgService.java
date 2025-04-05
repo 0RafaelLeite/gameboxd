dc500238af1635658de2142ea6fb6b0c3f59f86d
Rafael Leite <rafaelrocha.git@outlook.com>
2025-04-05 16:34:56 -0300

    add rawg service and specs

---
+++ backend/src/main/java/br/com/rafael/gameboxd/services/RawgService.java
@@ -0,0 +1,28 @@
+package br.com.rafael.gameboxd.services;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Service
+public class RawgService {
+
+    @Value("${rawg.api.key}")
+    private String apiKey;
+
+    @Value("${rawg.api.base-url}")
+    private String baseUrl;
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    public String getGames(String search, Integer page, Integer pagesize){
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(baseUrl + "/games")
+                .queryParam("key", apiKey)
+                .queryParam("search", search)
+                .queryParam("page", page)
+                .queryParam("pagesize", pagesize);
+
+        return restTemplate.getForObject(builder.toUriString(), String.class);
+    }
+}
